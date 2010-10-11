3ba02c0a7e9779c051520c070815482f2f369ec0
Olly Smith <olly.smith@gmail.com>
2010-10-11 23:23:53 +0800

    Add setuptools info so the library can be pip-installed

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='recurly-client-python',
+      version='1.2',
+      description='Recurly API client',
+      author='Recurly, Inc.',
+      author_email='support@recurly.com',
+      url='http://www.recurly.com',
+      packages=['recurly'],
+     )
