a760e60be3d5110d8321c7aa02210710cdbc0b65
Mark Paschal <markpasc@markpasc.org>
2011-10-27 16:58:12 -0700

    Fill out extra distribution metadata

--- setup.py
+++ setup.py
@@ -3,7 +3,19 @@ from distutils.core import setup
 setup(
     name='recurly',
     version='2.0.1dev',
-    packages=['recurly'],
+    description="Interact with Recurly's REST API for subscription management from your Python website",
+    author='Recurly',
+    author_email='support@recurly.com',
+    url='http://docs.recurly.com/client-libraries/python',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
 
+    packages=['recurly'],
     requires=['iso8601'],
 )
