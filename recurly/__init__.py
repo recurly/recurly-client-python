6177480f06770fe9efb577418864f03844b3a748
Mark Paschal <markpasc@markpasc.org>
2011-10-31 17:27:47 -0700

    Add helper function to decode push notification bodies

--- recurly/__init__.py
+++ recurly/__init__.py
@@ -501,3 +501,20 @@ class SubscriptionAddOn(Resource):
 
 
 Resource._learn_nodenames(locals().values())
+
+
+def objects_for_push_notification(notification):
+    """Decode a push notification with the given body XML.
+
+    Returns a dictionary containing the constituent objects of the push
+    notification. The kind of push notification is given in the ``"type"``
+    member of the returned dictionary.
+
+    """
+    notification_el = ElementTree.fromstring(notification)
+    objects = {'type': notification_el.tag}
+    for child_el in notification_el:
+        tag = child_el.tag
+        res = Resource.value_for_element(child_el)
+        objects[tag] = res
+    return objects
--- tests/test_recurly.py
+++ tests/test_recurly.py
@@ -16,6 +16,45 @@ class TestRecurly(unittest.TestCase):
         account_xml = ElementTree.tostring(account.to_element(), encoding='UTF-8')
         self.assertEqual(account_xml, xml('<account><username>importantbreakfast</username></account>'))
 
+    def test_objects_for_push_notification(self):
+        import recurly
+
+        objs = recurly.objects_for_push_notification("""<?xml version="1.0" encoding="UTF-8"?>
+        <new_subscription_notification>
+          <account>
+            <account_code>verena@test.com</account_code>
+            <username>verena</username>
+            <email>verena@test.com</email>
+            <first_name>Verena</first_name>
+            <last_name>Test</last_name>
+            <company_name>Company, Inc.</company_name>
+          </account>
+          <subscription>
+            <plan>
+              <plan_code>bronze</plan_code>
+              <name>Bronze Plan</name>
+              <version type="integer">2</version>
+            </plan>
+            <state>active</state>
+            <quantity type="integer">2</quantity>
+            <total_amount_in_cents type="integer">2000</total_amount_in_cents>
+            <activated_at type="datetime">2009-11-22T13:10:38-08:00</activated_at>
+            <canceled_at type="datetime"></canceled_at>
+            <expires_at type="datetime"></expires_at>
+            <current_period_started_at type="datetime">2009-11-22T13:10:38-08:00</current_period_started_at>
+            <current_period_ends_at type="datetime">2009-11-29T13:10:38-08:00</current_period_ends_at>
+            <trial_started_at type="datetime">2009-11-22T13:10:38-08:00</trial_started_at>
+            <trial_ends_at type="datetime">2009-11-29T13:10:38-08:00</trial_ends_at>
+          </subscription>
+        </new_subscription_notification>""")
+        self.assertEqual(objs['type'], 'new_subscription_notification')
+        self.assertTrue('account' in objs)
+        self.assertTrue(isinstance(objs['account'], recurly.Account))
+        self.assertEqual(objs['account'].username, 'verena')
+        self.assertTrue('subscription' in objs)
+        self.assertTrue(isinstance(objs['subscription'], recurly.Subscription))
+        self.assertEqual(objs['subscription'].state, 'active')
+
 
 if __name__ == '__main__':
     unittest.main()
