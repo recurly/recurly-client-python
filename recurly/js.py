1f5c9c768afa5b1c0e0b7c8523558dc91dc70c68
Mark Paschal <markpasc@markpasc.org>
2011-10-31 17:14:46 -0700

    Document the recurly.js functions

--- recurly/js.py
+++ recurly/js.py
@@ -10,10 +10,25 @@ PRIVATE_KEY = None
 
 
 class RequestForgeryError(Exception):
+    """An error raised when verification of a Recurly.js response fails."""
     pass
 
 
 def serialize(item):
+    """Serialize the given data according to Recurly.js signing rules.
+
+    If `item` is a list or tuple, it is serialized as ``[item]`` if it
+    contains one item, or ``[item,item,item]`` if it contains several. If
+    `item` is a dictionary, it is serialized as ``[key:value,key:value]`` for
+    alphanumeric keys or ``[value,value,value]`` if the keys are numeric only.
+    In either case, the values are serialized in natural ("asciibetical") sort
+    order by key.
+
+    Other values are serialized as their string values with special characters
+    used in serializing lists and dicts (the ``[],:\`` characters) escaped
+    with backslashes.
+
+    """
     if isinstance(item, list) or isinstance(item, tuple):
         return '[%s]' % ','.join(serialize(x) for x in item)
     elif isinstance(item, dict):
@@ -26,6 +41,12 @@ def serialize(item):
 
 
 def sign_params(claim, params, timestamp=None):
+    """Sign the given Recurly.js claim with the given parameters.
+
+    If `timestamp` is not provided, the current time is used. If the module's
+    ``PRIVATE_KEY`` value has not been set, a `ValueError` is raised.
+
+    """
     if PRIVATE_KEY is None:
         raise ValueError("Recurly.js private key is not set")
 
@@ -41,6 +62,23 @@ def sign_params(claim, params, timestamp=None):
 
 
 def verify_params(claim, params, signature=None):
+    """Verifies the given Recurly.js claim with the given parameters.
+
+    If the signature is verified, the function returns. If the signature is
+    determined to be invalid, a `RequestForgeryError` exception is raised.
+    Some possible reasons for the signature to be invalid are:
+
+    * the signature's timestamp is not present or not a timestamp
+    * the signature's timestamp is more than one hour old or one hour in the
+      future
+    * the signature does not match the signature calculated for the given
+      parameters with the configured ``PRIVATE_KEY``
+
+    If `signature` is not provided, the ``"signature"`` member of the `params`
+    mapping is removed and used instead. (One of these must be present, or a
+    `RequestForgeryError` is raised.)
+
+    """
     params = dict(params)
     if signature is None:
         try:
@@ -67,10 +105,18 @@ def verify_params(claim, params, signature=None):
 
 
 def sign_billing_info_update(account_code, timestamp=None):
+    """Sign a billing info update Recurly.js request for the given account code."""
     return sign_params('billinginfoupdate', {'account_code': account_code}, timestamp=timestamp)
 
 
 def sign_transaction(amount_in_cents, currency=recurly.DEFAULT_CURRENCY, account_code=None, timestamp=None):
+    """Sign a transaction creation Recurly.js request for the given amount.
+
+    If `currency` is not given, the default currency (as set in
+    ``recurly.DEFAULT_CURRENCY``) is used. If an `account_code` is not given,
+    the request is signed for a one-time transaction with no account attached.
+
+    """
     params = {
         'amount_in_cents': amount_in_cents,
         'currency': currency,
@@ -81,12 +127,18 @@ def sign_transaction(amount_in_cents, currency=recurly.DEFAULT_CURRENCY, account
 
 
 def verify_billing_info_update(params):
+    """Verify a billing info updated Recurly.js response with the given
+    parameters."""
     return verify_params('billinginfoupdated', params)
 
 
 def verify_transaction(params):
+    """Verify a transaction created Recurly.js response with the given
+    parameters."""
     return verify_params('transactioncreated', params)
 
 
 def verify_subscription(params):
+    """Verify a subscription created Recurly.js response with the given
+    parameters."""
     return verify_params('subscriptioncreated', params)
