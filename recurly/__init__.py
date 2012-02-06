f7c1ef33a72b84135d895d9a9a04fb7861a9741e
Mark Paschal <markpasc@markpasc.org>
2012-02-06 16:09:02 -0500

    Mark as 2.0.4 final

--- recurly/__init__.py
+++ recurly/__init__.py
@@ -19,7 +19,7 @@ http://docs.recurly.com/api/
 """
 
 
-__version__ = '2.0.4dev'
+__version__ = '2.0.4'
 
 BASE_URI = 'https://api.recurly.com/v2/'
 """The API endpoint to send requests to."""
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='recurly',
-    version='2.0.4dev',
+    version='2.0.4',
     description="Interact with Recurly's REST API for subscription management from your Python website",
     author='Recurly',
     author_email='support@recurly.com',
