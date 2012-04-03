c3eeb6499d2766a81f84381c98d97fc618e09b44
Wojciech Malinowski <wojciech.malinowski@gmail.com>
2012-04-03 23:37:22 +0200

    Automated installation, building and testing with the use of setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+import os.path
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as README:
+    DESCRIPTION = README.read()
 
 setup(
     name='recurly',
     version='2.1.1',
     description="Interact with Recurly's REST API for subscription management from your Python website",
+    long_description=DESCRIPTION,
     author='Recurly',
     author_email='support@recurly.com',
     url='http://docs.recurly.com/client-libraries/python',
+    license='MIT',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
@@ -15,7 +21,10 @@ setup(
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
     ],
-
     packages=['recurly'],
-    requires=['iso8601'],
+    install_requires=['iso8601'],
+    tests_require=['mock',
+                   'unittest2'],
+    test_suite='unittest2.collector',
+    zip_safe=True,
 )
