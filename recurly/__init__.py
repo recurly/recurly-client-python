9f3f505906b1946f0484420c5b0126a33a42ee62
Benjamin Eckel <bhelx@simst.im>
2017-04-03 13:31:03 -0500

    Bump api v2.6

--- recurly/__init__.py
+++ recurly/__init__.py
@@ -42,7 +42,7 @@ SUBDOMAIN = 'api'
 API_KEY = None
 """The API key to use when authenticating API requests."""
 
-API_VERSION = '2.5'
+API_VERSION = '2.6'
 """The API version to use when making API requests."""
 
 CA_CERTS_FILE = None
