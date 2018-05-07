e42c8d64634a9e4ce5f7b59212acaa14d08f1657
Benjamin Eckel <bhelx@simst.im>
2018-05-07 13:50:26 -0500

    Bump version 2.6.3

--- recurly/__init__.py
+++ recurly/__init__.py
@@ -20,7 +20,7 @@ https://dev.recurly.com/docs/getting-started
 
 """
 
-__version__ = '2.6.2'
+__version__ = '2.6.3'
 __python_version__ = '.'.join(map(str, sys.version_info[:3]))
 
 cached_rate_limits = {
