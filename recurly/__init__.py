6fccb5c92d609de957ec4e87454ba99223d204fa
Aaron Suarez <asuarez@recurly.com>
2019-01-17 14:57:38 -0600

    Bump to version 2.9.3

--- recurly/__init__.py
+++ recurly/__init__.py
@@ -21,7 +21,7 @@ https://dev.recurly.com/docs/getting-started
 
 """
 
-__version__ = '2.9.2'
+__version__ = '2.9.3'
 __python_version__ = '.'.join(map(str, sys.version_info[:3]))
 
 cached_rate_limits = {
