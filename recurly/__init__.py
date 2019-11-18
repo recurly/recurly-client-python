9a3adc30fc0fdbd2f1cbddf84140d87072567d38
Benjamin Eckel <bhelx@simst.im>
2019-11-18 11:45:01 -0600

    Release 3.2.0

--- recurly/__init__.py
+++ recurly/__init__.py
@@ -6,7 +6,7 @@ import glob
 import ssl
 import sys
 
-__version__ = "3.1.0"
+__version__ = "3.2.0"
 __python_version__ = ".".join(map(str, sys.version_info[:3]))
 
 USER_AGENT = "recurly-python/%s; python %s; %s" % (
