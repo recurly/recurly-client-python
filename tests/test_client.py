64e47a8110f4ca6a504e634aa54105a61caade15
Benjamin Eckel <bhelx@simst.im>
2020-04-27 11:56:58 -0500

    Fix python3 deprecation warnings
    Resolves #391
    
    Ran [pyupgrade](https://github.com/asottile/pyupgrade/) to look for
    deprecated patterns.

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,4 +5,4 @@ from recurly import Client
 class TestClient(unittest.TestCase):
     def test_api_version(self):
         client = Client("apikey")
-        self.assertRegex(client.api_version(), "v\d{4}-\d{2}-\d{2}")
+        self.assertRegex(client.api_version(), r"v\d{4}-\d{2}-\d{2}")
