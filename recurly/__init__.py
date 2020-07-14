09f878862dcc3e0b376f79ed64e4305c7acefb2d
Joanna Sese <jmgsese@gmail.com>
2020-07-14 16:52:53 -0500

    Add BECS support

--- recurly/__init__.py
+++ recurly/__init__.py
@@ -46,7 +46,7 @@ SUBDOMAIN = 'api'
 API_KEY = None
 """The API key to use when authenticating API requests."""
 
-API_VERSION = '2.27'
+API_VERSION = '2.28'
 """The API version to use when making API requests."""
 
 CA_CERTS_FILE = None
@@ -416,6 +416,7 @@ class BillingInfo(Resource):
         'transaction_type',
         'iban',
         'sort_code',
+        'bsb_code'
     )
     sensitive_attributes = ('number', 'verification_value', 'account_number', 'iban')
     xml_attribute_attributes = ('type',)
