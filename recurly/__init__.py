cd98971bc943d3a01d7a1341ce13e34aa04303ba
Joanna Sese <jmgsese@gmail.com>
2021-02-22 11:02:15 -0600

    Bump to version 2.9.24 and update changelog

--- recurly/__init__.py
+++ recurly/__init__.py
@@ -22,7 +22,7 @@ https://dev.recurly.com/docs/getting-started
 
 """
 
-__version__ = '2.9.23'
+__version__ = '2.9.24'
 __python_version__ = '.'.join(map(str, sys.version_info[:3]))
 
 cached_rate_limits = {
