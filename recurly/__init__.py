33f3c3c8d25ce358a1520e825e2c262e380d3d51
Emmanuel Feygelson <efeygelson@recurly.com>
2022-02-28 16:49:01 -0500

    v2 tax inclusive pricing adjustment

--- recurly/__init__.py
+++ recurly/__init__.py
@@ -883,6 +883,7 @@ class Adjustment(Resource):
         'total_in_cents',
         'currency',
         'tax_exempt',
+        'tax_inclusive'
         'tax_code',
         'tax_details',
         'start_date',
