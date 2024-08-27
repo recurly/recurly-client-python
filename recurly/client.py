3cb8e8e8abe5b19a30d542e0b58666f5d6d1fb2a
Recurly Integrations <dx@recurly.com>
2024-08-27 17:55:48 +0000

    Generated Latest Changes for v2021-02-25

--- recurly/client.py
+++ recurly/client.py
@@ -2863,7 +2863,7 @@ class Client(BaseClient):
         ----------
 
         external_subscription_id : str
-            External subscription ID or external_id. For ID no prefix is used e.g. `e28zov4fw0v2`. For external_id use prefix `external-id-`, e.g. `external-id-123456`.
+            External subscription ID, external_id or uuid. For ID no prefix is used e.g. `e28zov4fw0v2`. For external_id use prefix `external-id-`, e.g. `external-id-123456` and for uuid use prefix `uuid-` e.g. `uuid-7293239bae62777d8c1ae044a9843633`.
 
         Keyword Arguments
         -----------------
